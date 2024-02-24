65155ba01406e718c5ad438798f7c604a1ad0f91
E3BB <78990896+E3BB@users.noreply.github.com>
2024-02-23 23:01:29 -0500

    Changed main.py. End of 2/23/24 coding. - Elliott

--- main.py
+++ main.py
@@ -7,21 +7,25 @@ running = True
 
 disp = Display()
 
+"""def clamp(numa, maxm, minm):
+    num = min(numa, maxm)
+    num = max(numa, minm)
+    return num"""
+
 while running:  
     
-    inp = str(input())
-    
+    inp = str(input()).split()
+
     try:
-        int(inp[0])
-        int(inp[2])
-        str(inp[4])
+        next_x = int(inp[0]) - 1
+        next_y = int(inp[1]) - 1
     except:
         running = False
         break
+    next_char = inp[2]
 
-    next_x = int(inp[0])
-    next_y = int(inp[2])
-    next_char = inp[4]
+    """next_x = clamp(next_x,1,disp.width)
+    next_y = clamp(next_y,1,disp.height)"""
 
     disp.wipe()
     disp.change_pixel(next_x,next_y,next_char)
